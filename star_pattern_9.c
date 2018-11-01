eed6016bd9864836e056ad05b0f5ca97c0a6733c
Pratikkshirsagar <meetpratiik@gmail.com>
2018-11-01 18:25:14 +0530

    pattern added.

---
+++ star_pattern_9.c
@@ -0,0 +1,36 @@
+// --- Directions
+// prints a star pattern
+// 
+// --- Examples
+//
+// ABCDCBA
+// ABC CBA
+// AB   BA
+// A     A
+
+
+
+
+#include<stdio.h>
+
+int main()
+{
+	char k;
+	for(int i=1; i<=4; i++){
+		k='A';
+		for(int j=1; j<=7;j++){
+			if(j<=5-i || j>=3+i){
+
+				printf("%c", k);	
+				j<4?k++:k--;
+			}
+			else{
+				printf(" ");
+				if(j==4)
+					k--;
+			}
+		}
+		printf("\n");
+
+	}
+}
